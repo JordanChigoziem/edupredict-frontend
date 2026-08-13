3b60da2cdf4852b5cb53f9452f8460b25f504f96
JordanChigoziem <davejordan006@gmail.com>
2026-08-13 23:41:36 +0100

    Apply saved theme on page load

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,9 @@ import './index.css'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import { initTheme } from './utils/theme'
+
+initTheme()
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
